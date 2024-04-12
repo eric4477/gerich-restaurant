0bba401b470c0d9b028668607d7947eae1f4ecba
Eric <ericgeorge4477@gmail.com>
2024-04-12 23:49:29 +0200

    finished navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <h1>App</h1>
-    </div>
-  );
-}
+import {
+  AboutUs,
+  Chef,
+  FindUs,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
+} from "./container";
+import { Navbar } from "./components";
+
+const App = () => (
+  <div>
+    <Navbar />
+    {/* <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer /> */}
+  </div>
+);
 
 export default App;
