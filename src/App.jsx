fc30cbfd3138a7f3c240180b9996fc5e9b97ca5f
Eric <ericgeorge4477@gmail.com>
2024-04-16 00:38:17 +0200

    finished chef

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,8 @@ const App = () => (
     <Header />
     <AboutUs />
     <SpecialMenu />
-    {/* <Chef />
-    <Intro />
+    <Chef />
+    {/* <Intro />
     <Laurels />
     <Gallery />
     <FindUs />
